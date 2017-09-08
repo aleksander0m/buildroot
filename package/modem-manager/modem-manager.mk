7b8e898b908daa48f3e274849cb7a2761454737c
Aleksander Morgado <aleksander@aleksander.es>
2017-09-08 18:22:44 +0200

    modem-manager: build from git master
    Signed-off-by: Aleksander Morgado <aleksander@aleksander.es>

--- package/modem-manager/modem-manager.mk
+++ package/modem-manager/modem-manager.mk
@@ -4,13 +4,15 @@
 #
 ################################################################################
 
-MODEM_MANAGER_VERSION = 1.6.8
-MODEM_MANAGER_SOURCE = ModemManager-$(MODEM_MANAGER_VERSION).tar.xz
-MODEM_MANAGER_SITE = http://www.freedesktop.org/software/ModemManager
+MODEM_MANAGER_VERSION = master
+MODEM_MANAGER_SITE = git://anongit.freedesktop.org/ModemManager/ModemManager
 MODEM_MANAGER_LICENSE = GPL-2.0+ (programs, plugins), LGPL-2.0+ (libmm-glib)
-MODEM_MANAGER_LICENSE_FILES = COPYING
-MODEM_MANAGER_DEPENDENCIES = host-pkgconf host-intltool libglib2 libgudev
+MODEM_MANAGER_LICENSE_FILES = COPYING COPYING.LIB
+MODEM_MANAGER_DEPENDENCIES = host-pkgconf host-gettext libglib2 libgudev
+MODEM_MANAGER_AUTORECONF = YES
 MODEM_MANAGER_INSTALL_STAGING = YES
+
+BR_NO_CHECK_HASH_FOR += modem-manager-$(MODEM_MANAGER_VERSION).tar.gz
 
 ifeq ($(BR2_PACKAGE_MODEM_MANAGER_LIBQMI),y)
 MODEM_MANAGER_DEPENDENCIES += libqmi
